5b13e5d8db8c85147f2f3da5106d2c450704bde0
BhaskarDekaRaja <bhaskarthe4@gmail.com>
2019-05-29 01:00:03 +0530

    290519molecular_project

--- src/app/crop-commodities/crop-commodities.component.ts
+++ src/app/crop-commodities/crop-commodities.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-crop-commodities',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CropCommoditiesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) 
+  { 
+    this.getJsonLevel1().subscribe(
+      result=>{
+       this.commodities.push(result);
+      //  console.log(this.commodities[0].children[0].child)
+      console.log(this.commodities[0].child)
+      }
+    );
+
+    this.getJsonlevel2().subscribe(
+      result =>{
+        this.relation.push(result);
+        // console.log(this.relation[0].child);
+      }
+    )
+  }
 
   ngOnInit() {
   }
@@ -17,4 +34,30 @@ show = false;
   toggle(){
     this.show = !this.show;
     }
+
+    commodities = [];
+    relation = [];
+    // dispaly = false;
+  
+    openCommodities(arg0){
+      this.commodities[arg0].child = !this.commodities[arg0].child;
+      
+     }
+  
+     openChildren(arg0,arg1){
+    this.commodities[arg0].children[arg1].child = !this.commodities[arg0].children[arg1].child
+     }
+  
+     openChild(arg0){
+       this.relation[arg0].child = !this.relation[arg0].child;
+      //  console.log(arg0) 
+     }
+
+    getJsonLevel1(){
+      return this.http.get("../assets/level1.json");
+     }
+   
+     getJsonlevel2(){
+       return this.http.get("../assets/level2.json");
+     }
 }
